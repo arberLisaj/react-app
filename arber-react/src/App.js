58da09b7049856077b06fa92d14b51ab637cd7a4
arber <arberlisaj1@gmail.com>
2022-06-30 13:42:25 +0200

    my website

---
+++ arber-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>this is a simple react app</h1>
+        <i class="bi bi-person"></i>
+      </header>
+    </div>
+  );
+}
+
+export default App;
